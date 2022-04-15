4d8461d28d5379fed79c29f67dd6e8a02770cac1
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-04-15 14:50:30 +0300

    Add PersonCandidate class

---
+++ Lab2CSharp/Models/PersonCandidate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2CSharp.Models
+{
+    class PersonCandidate
+    {
+        #region Fields
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime _birthday;
+        #endregion
+
+        #region Properties
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+        public DateTime Birthday
+        {
+            get
+            {
+                return _birthday;
+            }
+            set
+            {
+                _birthday = value;
+            }
+        }
+        #endregion
+    }
+}
