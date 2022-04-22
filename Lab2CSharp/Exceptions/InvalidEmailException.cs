2a2ab919bd00d6243b620c821f38feb5f3601222
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-04-22 20:38:20 +0300

    Add InvalidEmailException

---
+++ Lab2CSharp/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab3CSharp.Exceptions
+{
+    class InvalidEmailException : Exception
+    {
+
+        public InvalidEmailException()
+        {
+        }
+
+        public InvalidEmailException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidEmailException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+    }
+}
