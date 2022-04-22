3ee64e4a0f554c81648144d640b38e01771c69c2
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-04-22 20:36:40 +0300

    Add FarDateOfBirthException

---
+++ Lab2CSharp/Exceptions/FarDateOfBirthExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab3CSharp.Exceptions
+{
+    class FarDateOfBirthException : Exception
+    {
+
+        public FarDateOfBirthException()
+        {
+        }
+
+        public FarDateOfBirthException(string message)
+            : base(message)
+        {
+        }
+
+        public FarDateOfBirthException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+    }
+}
