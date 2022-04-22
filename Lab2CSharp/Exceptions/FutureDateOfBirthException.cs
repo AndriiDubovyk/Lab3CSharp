19ed07ee28afc826823bc2b3acf993dbe9929cc8
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-04-22 20:34:55 +0300

    Add FutureDateOfBirthException

---
+++ Lab2CSharp/Exceptions/FutureDateOfBirthException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab3CSharp.Exceptions
+{
+    class FutureDateOfBirthException : Exception
+    {
+
+        public FutureDateOfBirthException()
+        {
+        }
+
+        public FutureDateOfBirthException(string message)
+            : base(message)
+        {
+        }
+
+        public FutureDateOfBirthException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+    }
+}
