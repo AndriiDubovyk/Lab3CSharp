963ac48343d5017e712d980f1ce0bb4f3b6a033b
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-04-22 21:25:04 +0300

    Fix email validation

--- Lab2CSharp/Models/Person.cs
+++ Lab2CSharp/Models/Person.cs
@@ -129,7 +129,7 @@ namespace Lab2CSharp.Models
         {
             string pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|" + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)" + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
             var regex = new Regex(pattern, RegexOptions.IgnoreCase);
-            if(regex.IsMatch(Email)) throw new InvalidEmailException();
+            if(!regex.IsMatch(Email)) throw new InvalidEmailException();
         }
     }
 }
